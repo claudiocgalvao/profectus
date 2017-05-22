538e17db90101b072d40428e673fd55ed474220a
claudiocgalvao <claudiocgalvao@gmail.com>
2017-05-22 09:03:00 -0300

    teste listar e incluir 2

---
+++ profectus-business/src/main/java/br/com/gti/profectus/business/dao/user/UserDAO.java
@@ -0,0 +1,8 @@
+package br.com.gti.profectus.business.dao.user;
+
+import br.com.gti.profectus.business.dao.ISpringDataDAO;
+import br.com.gti.profectus.business.entity.security.User;
+
+public interface UserDAO extends ISpringDataDAO<User, String> {
+
+}
